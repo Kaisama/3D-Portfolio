2315c1ff21c303e8008e157f5a5e9e92aac3e4b6
Akshat Bisht <bishtakshat64@gmail.com>
2024-06-01 21:57:06 +0530

    app jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+
+import Navbar from './components/Navbar'
+
+import Hero from './components/Hero'
+import About from './components/About'
+import Experience from './components/Experience'
+import Tech from './components/Tech'
+import Works from './components/Works'
+import Feedbacks from './components/Feedbacks'
+import Contact from './components/Contact'
+import StarsCanvas from './components/StarsCanvas'
 
 function App() {
  
   return (
-    <>
-     Hello
-    </>
+    <BrowserRouter>
+    <div className='relative z-0 bg-primary'>
+      <div className='bg-center bg-no-repeat bg-cover bg-hero-pattern'>
+        <Navbar/>
+        <Hero/>
+      </div>
+      <About/>
+      <Experience/>
+      <Tech/>
+      <Works/>
+      <Feedbacks/>
+      <div className='relative z-0'>
+        <Contact/>
+        <StarsCanvas/>
+      </div>
+    </div>
+    </BrowserRouter>
   )
 }
 
