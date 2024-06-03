0c1a89ca08080c63130f38510267c0dbd64b2631
Akshat Bisht <bishtakshat64@gmail.com>
2024-06-03 09:46:38 +0530

    pc is visible with 3d effect

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,3 @@
-import { motion } from "framer-motion"
 import { styles } from "../styles"
 import { ComputersCanvas } from "./canvas"
 
@@ -7,9 +6,16 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5 `}>
-        <div className="flex flex-col items-center justify-center mt-5"></div>
+        <div className="flex flex-col items-center justify-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]"/>
+          <div className="w-1 h-40 sm:h-80 violet-gradient"/>
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I am <span className="text-[#915eff]">Akshat Bisht</span></h1>
+          <p className={`${styles.heroSubText} text-white`}>I develop User Interfaces,Frontend and Backend Projects [MERN] with API Production and Testing  </p>
+        </div>
       </div>
-
+        <ComputersCanvas/>
     </section>
   )
 }
