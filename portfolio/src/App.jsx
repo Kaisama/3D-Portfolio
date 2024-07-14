e2567c2659f1a883aeb1f8c5e342b6e18d4470d5
Akshat Bisht <109125241+Kaisama@users.noreply.github.com>
2024-07-15 00:06:09 +0530

    Add files via upload

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,38 +1,27 @@
+import { BrowserRouter } from "react-router-dom";
 
-import { BrowserRouter } from 'react-router-dom'
-import './App.css'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import About from './components/About'
-import Experience from './components/Experience'
-import Tech from './components/Tech'
-import Works from './components/Works'
-import Feedbacks from './components/Feedbacks'
-import Contact from './components/Contact'
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
-
-
-function App() {
- 
+const App = () => {
   return (
     <BrowserRouter>
-    <div className='relative z-0 bg-primary'>
-      <div className='bg-center bg-no-repeat bg-cover bg-hero-pattern'>
-        <Navbar/>
-        <Hero/>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
+        </div>
       </div>
-      <About/>
-      <Experience/>
-      <Tech/>
-      <Works/>
-      <Feedbacks/>
-      <div className='relative z-0'>
-        <Contact/>
-        
-      </div>
-    </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
